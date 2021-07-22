b7b928199e4b789a539e2b3533f4e9678f53e52a
Artsiom Panko <artsiom.panko@gmail.com>
2021-07-22 16:26:05 +0300

    init project

---
+++ src/main/java/com/panko/oneTouchTestApp/OneTouchTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.panko.oneTouchTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTouchTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTouchTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panko/oneTouchTestApp/OneTouchTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panko.oneTouchTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTouchTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
